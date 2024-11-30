a33f8eba32462786c2fb66b2517e71343795f336
sunilpowar5 <sunilpowar523@gmail.com>
2024-11-30 12:59:02 +0530

    Update application.py

--- application.py
+++ application.py
@@ -41,4 +41,4 @@ def predict_datapoint():
         return render_template('home.html',results=np.round(results[0],3))
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=8080)
